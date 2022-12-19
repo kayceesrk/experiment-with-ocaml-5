516239ecb51d9576ff192be072d992693e25c34d
KC Sivaramakrishnan <kc@kcsrk.info>
2022-12-19 11:28:16 +0530

    updates

--- bench/bench.ml
+++ bench/bench.ml
@@ -16,60 +16,64 @@ module Benchmark_id = struct
   include Sexpable.To_stringable (T)
 end
 
+module Memo = struct
+  let memo ~get ~set f v =
+    match get v with
+    | None ->
+        let res = f v in
+        set v (Some res);
+        res
+    | Some res -> res
+
+  let memo_rec ~get ~set f_norec =
+    let f_ref = ref (fun _ -> assert false) in
+    let f_rec_memo = memo ~get ~set (fun x -> f_norec !f_ref x) in
+    f_ref := f_rec_memo;
+    f_rec_memo
+end
+
 module Sequential = struct
-  let rec edit_distance table s t =
+  let edit_distance f (s, t) =
     match String.length s, String.length t with
     | 0, x | x, 0 -> x
     | len_s, len_t ->
-        begin match table.(len_s).(len_t) with
-        | Some v -> v
-        | None ->
-          let s' = String.drop_suffix s 1 in
-          let t' = String.drop_suffix t 1 in
-          let cost_to_drop_both =
-            if Char.( = ) s.[len_s - 1] t.[len_t - 1] then 0 else 1
-          in
-          let d1 = edit_distance table s' t + 1 in
-          let d2 = edit_distance table s t' + 1 in
-          let d3 = edit_distance table s' t' + cost_to_drop_both in
-          let ( ++ ) = Int.min in
-          let res = d1 ++ d2 ++ d3 in
-          table.(len_s).(len_t) <- Some res;
-          res
-        end
+        let s' = String.drop_suffix s 1 in
+        let t' = String.drop_suffix t 1 in
+        let cost_to_drop_both =
+          if Char.( = ) s.[len_s - 1] t.[len_t - 1] then 0 else 1
+        in
+        let d1 = f (s', t) + 1 in
+        let d2 = f (s, t') + 1 in
+        let d3 = f (s', t') + cost_to_drop_both in
+        let ( ++ ) = Int.min in
+        d1 ++ d2 ++ d3
 end
 
 module Parallel = struct
   module Task = Domainslib.Task
 
-  let rec edit_distance table pool s t =
+  let edit_distance f (seq_ed, pool, s, t) =
     let async f = Task.async pool f in
     let await x = Task.await pool x in
     match String.length s, String.length t with
     | 0, x | x, 0 -> x
-    | len_s, len_t when len_s < 10 && len_t < 10 ->
-        Sequential.edit_distance table s t
+    | len_s, len_t when len_s < 10 && len_t < 10 -> seq_ed (s, t)
     | len_s, len_t ->
-        begin match table.(len_s).(len_t) with
-        | Some v -> v
-        | None ->
-            let s' = String.drop_suffix s 1 in
-            let t' = String.drop_suffix t 1 in
-            let cost_to_drop_both =
-              if Char.( = ) s.[len_s - 1] t.[len_t - 1] then 0 else 1
-            in
-            let d1 = async (fun () -> edit_distance table pool s' t + 1) in
-            let d2 = async (fun () -> edit_distance table pool s t' + 1) in
-            let d3 = async (fun () -> edit_distance table pool s' t' + cost_to_drop_both) in
-            let ( ++ ) = Int.min in
-            let res = await d1 ++ await d2 ++ await d3 in
-            table.(len_s).(len_t) <- Some res;
-            res
-        end
+        let s' = String.drop_suffix s 1 in
+        let t' = String.drop_suffix t 1 in
+        let cost_to_drop_both =
+          if Char.( = ) s.[len_s - 1] t.[len_t - 1] then 0 else 1
+        in
+        let d1 = async (fun () -> f (seq_ed, pool, s', t) + 1) in
+        let d2 = async (fun () -> f (seq_ed, pool, s, t') + 1) in
+        let d3 = async (fun () -> f (seq_ed, pool, s', t') + cost_to_drop_both) in
+        let ( ++ ) = Int.min in
+        await d1 ++ await d2 ++ await d3
 
-  let edit_distance table ~num_domains s t =
+  let edit_distance ~get ~set ~num_domains seq_ed s t =
     let pool = Task.setup_pool ~num_domains () in
-    let res = Task.run pool (fun () -> edit_distance table pool s t) in
+    let ed = Memo.memo_rec ~get ~set edit_distance in
+    let res = Task.run pool (fun () -> ed (seq_ed, pool, s, t)) in
     Task.teardown_pool pool;
     res
   ;;
@@ -87,8 +91,11 @@ let () =
        match benchmark with
        | Distance (seq_or_par, a, b) ->
          let table = Array.make_matrix ~dimx:(String.length a + 1) ~dimy:(String.length b + 1) None in
+         let get (a,b) = table.(String.length a).(String.length b) in
+         let set (a,b) res = table.(String.length a).(String.length b) <- res in
+         let seq_ed = Memo.memo_rec ~get ~set Sequential.edit_distance in
          (match seq_or_par with
-          | Seq -> printf "%d\n" (Sequential.edit_distance table a b : int)
-          | Par -> printf "%d\n" (Parallel.edit_distance table ~num_domains:(num_domains - 1) a b : int)))
+          | Seq -> printf "%d\n" (seq_ed (a, b) : int)
+          | Par -> printf "%d\n" (Parallel.edit_distance ~get:(fun (_,_,a,b) -> get (a,b)) ~set:(fun (_,_,a,b) v -> set (a,b) v) ~num_domains:(num_domains - 1) seq_ed a b : int)))
   |> Command_unix.run
 ;;
