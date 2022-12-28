8ace9e087f7794e10a09d19c063a141b8815951d
KC Sivaramakrishnan <kc@kcsrk.info>
2022-12-28 11:50:31 +0530

    make suffix string faster

--- bench/bench.ml
+++ bench/bench.ml
@@ -18,12 +18,12 @@ end
 
 module Memo = struct
   let memo ~get ~set f v =
-    match get v with
-    | None ->
+    let res = get v in
+    if res = -1 then begin
         let res = f v in
-        set v (Some res);
+        set v res;
         res
-    | Some res -> res
+    end else res
 
   let memo_rec ~get ~set f_norec =
     let f_ref = ref (fun _ -> assert false) in
@@ -32,15 +32,26 @@ module Memo = struct
     f_rec_memo
 end
 
+module SuffixString = struct
+  type t = {str : String.t; len: int}
+
+  let mk s = {str = s; len = String.length s}
+  let length {len; _} = len
+  let drop_suffix {str; len} n = {str; len = len - n}
+  let get {str; _} n = str.[n]
+end
+
 module Sequential = struct
   let edit_distance f (s, t) =
-    match String.length s, String.length t with
+    match SuffixString.length s, SuffixString.length t with
     | 0, x | x, 0 -> x
     | len_s, len_t ->
-        let s' = String.drop_suffix s 1 in
-        let t' = String.drop_suffix t 1 in
+        let s' = SuffixString.drop_suffix s 1 in
+        let t' = SuffixString.drop_suffix t 1 in
         let cost_to_drop_both =
-          if Char.( = ) s.[len_s - 1] t.[len_t - 1] then 0 else 1
+          if Char.( = ) (SuffixString.get s (len_s - 1))
+                        (SuffixString.get t (len_t - 1))
+          then 0 else 1
         in
         let d1 = f (s', t) + 1 in
         let d2 = f (s, t') + 1 in
@@ -55,14 +66,16 @@ module Parallel = struct
   let edit_distance f (seq_ed, pool, s, t) =
     let async f = Task.async pool f in
     let await x = Task.await pool x in
-    match String.length s, String.length t with
+    match SuffixString.length s, SuffixString.length t with
     | 0, x | x, 0 -> x
-    | len_s, len_t when len_s < 10 && len_t < 10 -> seq_ed (s, t)
+    | len_s, len_t when len_s * len_t < 400_000_000 -> seq_ed (s, t)
     | len_s, len_t ->
-        let s' = String.drop_suffix s 1 in
-        let t' = String.drop_suffix t 1 in
+        let s' = SuffixString.drop_suffix s 1 in
+        let t' = SuffixString.drop_suffix t 1 in
         let cost_to_drop_both =
-          if Char.( = ) s.[len_s - 1] t.[len_t - 1] then 0 else 1
+          if Char.( = ) (SuffixString.get s (len_s - 1))
+                        (SuffixString.get t (len_t - 1))
+          then 0 else 1
         in
         let d1 = async (fun () -> f (seq_ed, pool, s', t) + 1) in
         let d2 = async (fun () -> f (seq_ed, pool, s, t') + 1) in
@@ -90,12 +103,24 @@ let () =
      fun () ->
        match benchmark with
        | Distance (seq_or_par, a, b) ->
-         let table = Array.make_matrix ~dimx:(String.length a + 1) ~dimy:(String.length b + 1) None in
-         let get (a,b) = table.(String.length a).(String.length b) in
-         let set (a,b) res = table.(String.length a).(String.length b) <- res in
-         let seq_ed = Memo.memo_rec ~get ~set Sequential.edit_distance in
-         (match seq_or_par with
-          | Seq -> printf "%d\n" (seq_ed (a, b) : int)
-          | Par -> printf "%d\n" (Parallel.edit_distance ~get:(fun (_,_,a,b) -> get (a,b)) ~set:(fun (_,_,a,b) v -> set (a,b) v) ~num_domains:(num_domains - 1) seq_ed a b : int)))
+           let a = SuffixString.mk a in
+           let b = SuffixString.mk b in
+           let table =
+             Array.make_matrix ~dimx:(SuffixString.length a + 1)
+             ~dimy:(SuffixString.length b + 1) (-1)
+           in
+           let get (a,b) =
+             table.(SuffixString.length a).(SuffixString.length b)
+             in
+           let set (a,b) res =
+             table.(SuffixString.length a).(SuffixString.length b) <- res
+           in
+           let seq_ed = Memo.memo_rec ~get ~set Sequential.edit_distance in
+           (match seq_or_par with
+             | Seq -> printf "%d\n" (seq_ed (a, b) : int)
+             | Par -> printf "%d\n" (Parallel.edit_distance
+                 ~get:(fun (_,_,a,b) -> get (a,b))
+                 ~set:(fun (_,_,a,b) v -> set (a,b) v)
+                 ~num_domains:(num_domains - 1) seq_ed a b : int)))
   |> Command_unix.run
 ;;
